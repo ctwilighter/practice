c64e538fbf56738d3777b65f27664e62af150702
ctwilighter <1529726299@qq.com>
2016-10-28 12:17:47 +0800

    返回页面顶部实例

---
+++ javascript 回到顶部/scripts/addLoadEvent.js
@@ -0,0 +1,13 @@
+//添加加载函数
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			oldonload();
+			func();
+		}
+	}
+}
+addLoadEvent(backToTop);
