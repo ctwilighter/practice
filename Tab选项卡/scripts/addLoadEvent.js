77c0caa0ffc00c077a2de8840788e9b0ea6e519e
ctwilighter <1529726299@qq.com>
2016-10-28 17:10:35 +0800

    Tab选项卡的切换

---
+++ Tab选项卡/scripts/addLoadEvent.js
@@ -0,0 +1,14 @@
+//添加加载函数
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			oldonload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(changeTab);
